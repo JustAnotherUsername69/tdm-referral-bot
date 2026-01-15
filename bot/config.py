f739db1c994d5d14ca72d42012f36d48b8425de9
Akshay Bangad <68388096+JustAnotherUsername69@users.noreply.github.com>
2026-01-15 20:01:18 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,25 @@
+import os
+
+BOT_TOKEN = "8566082138:AAGQNIak_kCYaOgchdKZPaDUahpT8In9wJE"
+
+ADMIN_ID = 577188267
+CHANNEL_ID = -1001267968308
+
+COUPON_LOG_CHANNEL = -1003516164058
+BROADCAST_LOG_CHANNEL = -1003536597909
+USER_LOG_CHANNEL = -1003477822786
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "tdm_bot",
+    "user": "tdm_user",
+    "password": "Akshay"
+}
+
+REDEEM_INSTRUCTIONS = (
+    "📌 Coupon Usage Instructions\n"
+    "• Minimum order value: ₹100\n"
+    "• Valid for NEW BigBasket accounts only\n"
+    "• Coupons may show INVALID for old users\n"
+)
